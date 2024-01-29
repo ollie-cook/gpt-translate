ba004c0bc029f4637499df0cf808fe6dc1b2adc3
Oliver Cook <olliewatford@gmail.com>
2024-01-29 10:50:28 +0000

    Rebrand

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Form from "./components/Form";
 
 export default function HomePage() {
   return (
-    <main className="min-h-screen w-11/12 sm:w-3/4 lg:w-1/2 xl:w-1/3">
+    <main className="w-11/12 sm:w-3/4 lg:w-1/2 xl:w-1/3">
       <Form />
     </main>
   );
