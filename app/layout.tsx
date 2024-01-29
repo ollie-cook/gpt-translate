09ac2fc64301cc51ae4c74cda9a8ae83524d9c40
Oliver Cook <olliewatford@gmail.com>
2024-01-29 11:35:20 +0000

    centering the header

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} min-h-screen relative flex flex-col items-center`}>
-        <h1 className="text-5xl font-bold mt-4">Context Translate</h1>
+        <h1 className="text-5xl font-bold mt-4 text-center">Context Translate</h1>
         {children}
         <p className="absolute bottom-2 left-1/2 -translate-x-1/2">Built by <a href="https://www.olliecookie.com" className="underline" target="_blank">Ollie Cook</a>&#x1f36a;</p>
         <Analytics />
